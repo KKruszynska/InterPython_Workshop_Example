1001d9521686a5db841605ef0436445ee87e8d5a
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-04-05 02:30:38 -0700

    Style fixes.

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -2,14 +2,15 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-The lightcurves are saved in a table (2D array) where each row corresponds to a single observation. 
-Depending on the dataset (LSST or Kepler), a table can contain observations of a single or several objects, 
-in a single or different bands.
+The lightcurves are saved in a table (2D array) where each row corresponds to 
+a single observation. 
+Depending on the dataset (LSST or Kepler), a table can contain observations of 
+a single or several objects, in a single or different bands.
 """
 
 import pandas as pd
-import numpy as np
-from astropy.timeseries import LombScargle
+# import numpy as np
+# from astropy.timeseries import LombScargle
 
 def load_dataset(filename):
     """Load a table from CSV file.
@@ -33,7 +34,9 @@ def max_mag(data,mag_col):
 def min_mag(data,mag_col):
     """Calculate the min magnitude of a lightcurve.
     :param data: pd.DataFrame with observed magnitudes for a single source.
-    :param mag_col: a string with the name of the column for calculating the min value.
+    :param mag_col: a string with the name of the column for calculating 
+                    the min value.
     :returns: The min value of the column.
     """
     return data[mag_col].min()
+    

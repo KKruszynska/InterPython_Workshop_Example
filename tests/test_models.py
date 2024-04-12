a52452b116c3627261513f0fdcdd200651fdc256
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2024-04-12 04:59:36 -0700

    Parametrized tests for min_mag(), max_mag() and mean_mag()

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,29 +3,27 @@
 import pandas as pd
 import pytest
 
-def test_max_mag_integers():
-    # Test that max_mag function works for integers
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        7),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+    ])
+
+def test_max_mag(test_df, test_colname, expected):
+    """Test max function works for array of zeroes and positive integers."""
     from lcanalyzer.models import max_mag
-
-    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
-                                       [7, 8, 9], 
-                                       [3, 4, 1]], columns=list("abc"))
-    test_input_colname = "a"
-    test_output = 7
-
-    assert max_mag(test_input_df, test_input_colname) == test_output
-
-def test_max_mag_zeros():
-    # Test that max_mag function works for zeros
-    from lcanalyzer.models import max_mag
-
-    test_input_df = pd.DataFrame(data=[[0, 0, 0], 
-                                       [0, 0, 0], 
-                                       [0, 0, 0]], columns=list("abc"))
-    test_input_colname = "b"
-    test_output = 0
-
-    assert max_mag(test_input_df, test_input_colname) == test_output
+    assert max_mag(test_df, test_colname) == expected
 
 def test_max_mag_strings():
     # Test for TypeError when passing a string
@@ -35,62 +33,50 @@ def test_max_mag_strings():
     with pytest.raises(TypeError):
         error_expected = max_mag('string', test_input_colname)
 
-def test_mean_mag_integers():
-    # Test that mean mag works for ints
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                           [7, 8, 9], 
+                           [3, 4, 1]], columns=list("abc")),
+        "a",
+        pytest.approx(3.66, 0.01)),
+        
+        (pd.DataFrame(data=[[1, 1, 1], 
+                           [1, 1, 1], 
+                           [1, 1, 1]], columns=list("abc")),
+        "b",
+        pytest.approx(1.0, 0.01)),
+
+        (pd.DataFrame(data=[[-1, 1,- 1], 
+                           [1, -1, 1], 
+                           [0, 0, 0]], columns=list("abc")),
+        "b",
+        pytest.approx(0.0, 0.01)),
+    ])
+
+def test_mean_mag(test_df, test_colname, expected):
+    """Test max function works for array of zeroes and positive integers."""
     from lcanalyzer.models import mean_mag
+    assert mean_mag(test_df, test_colname) == expected
 
-    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
-                                       [7, 8, 9], 
-                                       [3, 4, 1]], columns=list("abc"))
-    test_input_colname = "a"
-    test_output = (1 + 7 + 3) / 3.
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                           [7, 8, 9], 
+                           [3, 4, 1]], columns=list("abc")),
+        "c",
+        1),
+        
+        (pd.DataFrame(data=[[-1, -5, -3], 
+                           [-7, -8, -9], 
+                           [-3, -4, -1]], columns=list("abc")),
+        "c",
+        -9),
+    ])
 
-    assert mean_mag(test_input_df, test_input_colname) == test_output
-
-def test_mean_mag_zeros():
-    # Test that mean_mag function works for ones
-    from lcanalyzer.models import mean_mag
-
-    test_input_df = pd.DataFrame(data=[[1, 1, 1], 
-                                       [1, 1, 1], 
-                                       [1, 1, 1]], columns=list("abc"))
-    test_input_colname = "b"
-    test_output = 1.0
-
-    assert mean_mag(test_input_df, test_input_colname) == test_output
-
-def test_mean_mag_negatives():
-    # Test that mean_mag function works for negative and positive numbers
-    from lcanalyzer.models import mean_mag
-
-    test_input_df = pd.DataFrame(data=[[-1, 1,- 1], 
-                                       [1, -1, 1], 
-                                       [0, 0, 0]], columns=list("abc"))
-    test_input_colname = "b"
-    test_output = 0.0
-
-    assert mean_mag(test_input_df, test_input_colname) == test_output
-
-def test_min_mag_integers():
-    # Test that min mag works for ints
+def test_min_mag(test_df, test_colname, expected):
+    """Test min function works for array of zeroes and positive integers."""
     from lcanalyzer.models import min_mag
-
-    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
-                                       [7, 8, 9], 
-                                       [3, 4, 1]], columns=list("abc"))
-    test_input_colname = "c"
-    test_output = 1
-
-    assert min_mag(test_input_df, test_input_colname) == test_output
-
-def test_min_mag_negatives():
-    # Test that min mag works for negatives
-    from lcanalyzer.models import min_mag
-
-    test_input_df = pd.DataFrame(data=[[-1, -5, -3], 
-                                       [-7, -8, -9], 
-                                       [-3, -4, -1]], columns=list("abc"))
-    test_input_colname = "c"
-    test_output = -9
-
-    assert min_mag(test_input_df, test_input_colname) == test_output
+    assert min_mag(test_df, test_colname) == expected
